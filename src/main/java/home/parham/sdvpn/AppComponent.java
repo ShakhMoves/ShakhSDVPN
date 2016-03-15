fc7eb5a55eba96031b46f6504c4bea76de3c11b9
Parham Alvani <parham.alvani@gmail.com>
2016-03-16 00:03:25 +0330

    Finally we found issue in our design :D

--- src/main/java/home/parham/sdvpn/AppComponent.java
+++ src/main/java/home/parham/sdvpn/AppComponent.java
@@ -15,16 +15,11 @@
  */
 package home.parham.sdvpn;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.apache.felix.scr.annotations.*;
 import org.onosproject.app.ApplicationService;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.net.flow.FlowRuleService;
 import org.onosproject.net.host.HostService;
-import org.onosproject.net.packet.PacketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,21 +35,16 @@ public class AppComponent {
 	protected ApplicationService applicationService;
 
 	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
-	private HostService switchService;
-
-	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
 	private HostService hostService;
 
 	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
 	protected FlowRuleService flowRuleService;
 
-	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
-	protected PacketService packetService;
 
 	@Activate
 	protected void activate() {
-		ApplicationId appId = applicationService.getId("me.elahe.msdvpn");
-		L2Switching l2Switching = new L2Switching(appId, hostService, flowRuleService);
+		ApplicationId appId = applicationService.getId("home.parham.sdvpn");
+		L2Switching l2Switching = new L2Switching(appId, flowRuleService);
 		hostService.addListener(l2Switching);
 
 		log.info("Started");
