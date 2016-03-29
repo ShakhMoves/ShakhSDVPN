6bceb1a7fde05e5bbaaec7e60ead713f4dfa7a78
Parham Alvani <parham.alvani@gmail.com>
2016-03-29 15:26:12 +0430

    SDVPNCommands class was added.

---
+++ src/main/java/home/parham/sdvpn/SDVPNCommands.java
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : ShakhSDVPN 
+ * 
+ * [] Package Name : home.parham.sdvpn
+ *
+ * [] Creation Date : 29-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package home.parham.sdvpn;
+
+import org.onosproject.cli.AbstractShellCommand;
+import org.apache.karaf.shell.commands.Command;
+
+public class SDVPNCommands extends AbstractShellCommand {
+	@Override
+	protected void execute() {
+
+	}
+}
