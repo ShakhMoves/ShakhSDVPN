55818b9267cbd1869e5d2820e944cf64cbf6b79d
Parham Alvani <parham.alvani@gmail.com>
2016-05-07 02:12:46 +0430

    Nothing ...

--- src/main/java/home/parham/sdvpn/SDVPNAddRuleCommand.java
+++ src/main/java/home/parham/sdvpn/SDVPNAddRuleCommand.java
@@ -19,7 +19,6 @@ import org.onosproject.cli.AbstractShellCommand;
 
 @Command(scope = "sdvpn", name = "add-rule", description = "Add rule for dropping packet at hosts")
 public class SDVPNAddRuleCommand extends AbstractShellCommand {
-
 	@Override
 	protected void execute() {
 		SDVPN sdvpn = get(SDVPN.class);
