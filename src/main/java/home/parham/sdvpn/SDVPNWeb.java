341b643d6ef6345fd9f0955c687bacd9e0241906
Parham Alvani <parham.alvani@gmail.com>
2016-05-16 17:25:52 +0430

    SDVPNWeb class was added.

---
+++ src/main/java/home/parham/sdvpn/SDVPNWeb.java
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : ShakhSDVPN 
+ * 
+ * [] Package Name : home.parham.sdvpn
+ *
+ * [] Creation Date : 16-05-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package home.parham.sdvpn;
+
+public class SDVPNWeb {
+
+}
