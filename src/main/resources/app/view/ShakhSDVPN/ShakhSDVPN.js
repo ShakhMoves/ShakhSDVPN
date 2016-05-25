93de66d107883e616470e62ebeb4b71e9e6af9db
Parham Alvani <parham.alvani@gmail.com>
2016-05-25 06:15:52 +0430

    Add table to show hosts :)

--- src/main/resources/app/view/ShakhSDVPN/ShakhSDVPN.js
+++ src/main/resources/app/view/ShakhSDVPN/ShakhSDVPN.js
@@ -30,40 +30,47 @@
         $scope.$apply();
     }
 
+    function hostEventCb(host) {
+        $scope.hosts.append(host);
+        $scope.$apply()
+    }
+
 
     angular.module('ovShakhSDVPN', [])
         .controller('OvShakhSDVPNCtrl',
-        ['$log', '$scope', 'WebSocketService', 'KeyService',
+            ['$log', '$scope', 'WebSocketService', 'KeyService',
 
-        function (_$log_, _$scope_, _wss_, _ks_) {
-            $log = _$log_;
-            $scope = _$scope_;
-            wss = _wss_;
-            ks = _ks_;
+                function (_$log_, _$scope_, _wss_, _ks_) {
+                    $log = _$log_;
+                    $scope = _$scope_;
+                    wss = _wss_;
+                    ks = _ks_;
 
-            var handlers = {};
-            $scope.data = {};
+                    var handlers = {};
+                    $scope.hosts = [];
+                    $scope.data = {};
 
-            // data response handler
-            handlers[dataResp] = respDataCb;
-            wss.bindHandlers(handlers);
+                    // data response handler
+                    handlers[dataResp] = respDataCb;
+                    handlers['hostEvent'] = hostEventCb;
+                    wss.bindHandlers(handlers);
 
-            addKeyBindings();
+                    addKeyBindings();
 
-            // custom click handler
-            $scope.getData = getData;
+                    // custom click handler
+                    $scope.getData = getData;
 
-            // get data the first time...
-            getData();
+                    // get data the first time...
+                    getData();
 
-            // cleanup
-            $scope.$on('$destroy', function () {
-                wss.unbindHandlers(handlers);
-                ks.unbindKeys();
-                $log.log('OvSampleCustomCtrl has been destroyed');
-            });
+                    // cleanup
+                    $scope.$on('$destroy', function () {
+                        wss.unbindHandlers(handlers);
+                        ks.unbindKeys();
+                        $log.log('OvShakhSDVPNCtrl has been destroyed');
+                    });
 
-            $log.log('OvSampleCustomCtrl has been created');
-        }]);
+                    $log.log('OvShakhSDVPNCtrl has been created');
+                }]);
 
 }());
